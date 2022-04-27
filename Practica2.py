862dda66944362b771a7c9af9522ec0ac7e43fff
Gylc87 <a19110230@ceti.mx>
2022-04-26 20:23:39 -0500

    Update Practica2.py

--- Practica2.py
+++ Practica2.py
@@ -2,13 +2,19 @@ import cv2
 import keyboard
 import numpy as np
 import matplotlib.pyplot as plt
+import PIL
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-
+global op
+op = 0
 image1 = cv2.imread("img1.jpg")
 image2 = cv2.imread("img2.jpg")
 
-img1 = cv2.resize(image1, dsize=(750, 550), interpolation=cv2.INTER_CUBIC)
-img2 = cv2.resize(image2, dsize=(750, 550), interpolation=cv2.INTER_CUBIC)
+img1 = cv2.resize(image1, dsize=(550, 350), interpolation=cv2.INTER_CUBIC)
+img2 = cv2.resize(image2, dsize=(550, 350), interpolation=cv2.INTER_CUBIC)
+
 
 suma = cv2.add(img1, img2)
 resta = cv2.subtract(img1, img2)
@@ -29,26 +35,28 @@ negacion = cv2.bitwise_not(img1, img2)
 
 ##conca_verti = cv2.hconcat([img1, img2])
 ##conca_hori = cv2.hconcat([suma, resta])
-##conca = cv2.vconcat([conca_verti, conca_hori])
+#conca = cv2.vconcat([conca_verti, conca_hori])
 
 
 while True:
     if keyboard.is_pressed("Y") or keyboard.is_pressed("y"):
         #cv2.imshow("Operaciones", conca)
         cv2.imshow("image1",img1), cv2.moveWindow("image1", 0, 0)
-        cv2.imshow("image2",img2), cv2.moveWindow("image2", 770 , 0)
-        cv2.imshow("Suma",suma), cv2.moveWindow("Suma", 400, 250)
-        cv2.imshow("Resta", resta), cv2.moveWindow("Resta", 400, 280)
-        cv2.imshow("Division", division), cv2.moveWindow("Division", 400, 310)
-        cv2.imshow("Multiplicacion", multi), cv2.moveWindow("Multiplicacion", 400, 340)
+        cv2.imshow("image2",img2), cv2.moveWindow("image2", 975 , 0)
+        cv2.imshow("Suma",suma), cv2.moveWindow("Suma", 500, 250)
+        cv2.imshow("Resta", resta), cv2.moveWindow("Resta", 500, 280)
+        cv2.imshow("Division", division), cv2.moveWindow("Division", 500, 310)
+        cv2.imshow("Multiplicacion", multi), cv2.moveWindow("Multiplicacion", 500, 340)
         ##cv2.imshow("Raiz", raiz), cv2.moveWindow("Raiz", 400, 430)
-        cv2.imshow("Potencia 2", potencia), cv2.moveWindow("Potencia 2", 400, 370)
-        cv2.imshow("Potencia 4", potencia2), cv2.moveWindow("Potencia 4", 400, 400)
-        cv2.imshow("Conjuncion", conjuncion), cv2.moveWindow("Conjuncion", 400, 430)
-        cv2.imshow("Disyuncion", disyuncion), cv2.moveWindow("Disyuncion", 400, 460)
-        cv2.imshow("Negacion", negacion), cv2.moveWindow("Negacion", 400, 490)
+        cv2.imshow("Potencia 2", potencia), cv2.moveWindow("Potencia 2", 500, 370)
+        cv2.imshow("Potencia 4", potencia2), cv2.moveWindow("Potencia 4", 500, 400)
+        cv2.imshow("Conjuncion", conjuncion), cv2.moveWindow("Conjuncion", 500, 430)
+        cv2.imshow("Disyuncion", disyuncion), cv2.moveWindow("Disyuncion", 500, 460)
+        cv2.imshow("Negacion", negacion), cv2.moveWindow("Negacion", 500, 490)
         ##cv2.imshow("Integral", integral), cv2.moveWindow("Integral", 400, 510)
         break
+
+
     
 cv2.waitKey(0)
 cv2.destroyAllWindows()
