2987473c2f028e1692964168a07fdebb516178c4
Gylc87 <a19110230@ceti.mx>
2022-04-26 22:38:24 -0500

    Update Practica2.py

--- Practica2.py
+++ Practica2.py
@@ -48,9 +48,8 @@ c = 255 / np.log(1 + np.max(img1))
 logn = c * (np.log(img1 + 1))
 logn = np.array(logn, dtype = np.uint8)
 
-
 transpuesta1 = cv2.transpose(img1)
-transpuesta2 = cv2.transpose(img2)
+
 
 
 ##conca_verti = cv2.hconcat([img1, img2])
@@ -62,26 +61,26 @@ while True:
     if keyboard.is_pressed("Y") or keyboard.is_pressed("y"):
         #cv2.imshow("Operaciones", conca)
         cv2.imshow("image1",img1), cv2.moveWindow("image1", 0, 0)
-        cv2.imshow("image2",img2), cv2.moveWindow("image2", 975 , 0)
+        cv2.imshow("image2",img4), cv2.moveWindow("image2", 975 , 0)
         cv2.imshow("Suma",suma), cv2.moveWindow("Suma", 500, 250)
         cv2.imshow("Resta", resta), cv2.moveWindow("Resta", 500, 280)
         cv2.imshow("Division", division), cv2.moveWindow("Division", 500, 310)
         cv2.imshow("Multiplicacion", multi), cv2.moveWindow("Multiplicacion", 500, 340)
-        cv2.imshow("Raiz", raiz), cv2.moveWindow("Raiz", 500, 510)
         cv2.imshow("Potencia 2", potencia), cv2.moveWindow("Potencia 2", 500, 370)
         cv2.imshow("Potencia 4", potencia2), cv2.moveWindow("Potencia 4", 500, 400)
         cv2.imshow("Conjuncion", conjuncion), cv2.moveWindow("Conjuncion", 500, 430)
         cv2.imshow("Disyuncion", disyuncion), cv2.moveWindow("Disyuncion", 500, 460)
         cv2.imshow("Negacion", negacion), cv2.moveWindow("Negacion", 500, 490)
+        cv2.imshow("Raiz", raiz), cv2.moveWindow("Raiz", 500, 510)
         cv2.imshow("Traslacion img1", traslacion1), cv2.moveWindow("Traslacion img1", 500, 540)
         cv2.imshow("Traslacion img2", traslacion2), cv2.moveWindow("Traslacion img2", 500, 570)
         cv2.imshow("Rotacion img1", rotacion1), cv2.moveWindow("Rotacion img1", 500, 600)
         cv2.imshow("Rotacion img2", rotacion2), cv2.moveWindow("Rotacion img2", 500, 630)
         cv2.imshow("Escalado", escalado), cv2.moveWindow("Escalado", 500, 660)
-        cv2.imshow("Logaritmo Natural img1", logn), cv2.moveWindow("Logaritmo Natural img1", 500, 690)
-        cv2.imshow("Logaritmo Natural img2", logn2), cv2.moveWindow("Logaritmo Natural img2", 500, 710)
-        cv2.imshow("Transpuesta img1", transpuesta1), cv2.moveWindow("Transpuesta img1", 500, 740)
-        cv2.imshow("Transpuesta img1", transpuesta2), cv2.moveWindow("Transpuesta img2", 500, 740)
+        cv2.imshow("Logaritmo Natural 1", logn), cv2.moveWindow("Logaritmo Natural 1", 500, 690)
+        ##cv2.imshow("Logaritmo Natural 2", logn2), cv2.moveWindow("Logaritmo Natural 2", 500, 710)
+        cv2.imshow("Transpuesta 1", transpuesta1), cv2.moveWindow("Transpuesta 1", 500, 740)
+       ## cv2.imshow("Transpuesta 2", transpuesta2), cv2.moveWindow("Transpuesta 2", 500, 740)
         break
 
 
